960507d5256c896fa085230c7bf7a1416f923699
shudal <shudal@qq.com>
2019-01-31 12:18:17 +0000

    c1

--- application/index/controller/Img.php
+++ application/index/controller/Img.php
@@ -6,17 +6,19 @@ use GuzzleHttp\Client;
 use Cache\Adapter\Chain\Chain\ChainCachePoo;
 
 class Img extends Base{
-    public function getBg(){
+    public function getImg(){
 
         $headers = [
             'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11'
         ];
 
         $client = new Client();
-        $res = $client->request('GET','http://sso.jwc.whut.edu.cn/Certification/toIndex.do',[],$headers);
+
+        $w=1920;$h=1080;
+        $res = $client->request('GET','https://wall.alphacoders.com/by_resolution.php',['w'=>$w,'h'=>h],$headers);
         $html = (string)$res->getBody();
 
-        $data = QueryList::html($html)->find('h3')->texts();
+        $data = QueryList::html($html)->find('body')->texts();
         dump($data);
     }
 }
